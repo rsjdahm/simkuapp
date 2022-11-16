df9029b6adaffd6cdb50bad8e703a2617c9741e4
walidsj <mohwalidas@outlook.com>
2022-11-16 09:50:51 +0800

    update table

--- resources/views/pages/parameter/global/unit_subunit/unit/table.blade.php
+++ resources/views/pages/parameter/global/unit_subunit/unit/table.blade.php
@@ -1,7 +1,7 @@
 <div class="alert alert-warning" role="alert">
     <div class="row">
         <div class="col-12">
-            <table class="table-responsive m-0 table">
+            <table class="table-responsive table-borderless m-0 table">
                 <tr>
                     <td class="py-2"><a data-action="back-tab" data-target="#bidang"
                             class="btn btn-primary btn-sm text-white"><i class="fas fa-backward"></i> Kembali</a></td>
