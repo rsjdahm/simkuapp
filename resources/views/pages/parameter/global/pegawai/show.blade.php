7b6c714279524873980d1e4e9e2a088e23fcd468
walidsj <mohwalidas@outlook.com>
2022-11-23 22:17:19 +0800

    update

--- resources/views/pages/parameter/global/pegawai/show.blade.php
+++ resources/views/pages/parameter/global/pegawai/show.blade.php
@@ -1,13 +1,13 @@
 <div class="alert alert-warning" role="alert">
     <div class="row">
         <div class="col-md-9">
-            <table class="table-responsive table-sm m-0 table">
+            <table class="table-responsive table-sm table-borderless m-0 table">
                 <tr>
-                    <td class="py-2"><a data-action="back-tab" data-target="#pegawai"
-                            class="btn btn-primary btn-sm text-white"><i class="fas fa-backward"></i> Kembali</a></td>
-                    <td class="py-2">Nama Lengkap</td>
-                    <td class="py-2">:</td>
-                    <th class="py-2">{{ $pegawai->nama }}</th>
+                    <td><a data-action="back-tab" data-target="#pegawai" class="btn btn-primary btn-sm text-white"><i
+                                class="fas fa-backward"></i> Kembali</a></td>
+                    <td>Nama Lengkap</td>
+                    <td>:</td>
+                    <th>{{ $pegawai->nama }}</th>
                 </tr>
             </table>
         </div>
