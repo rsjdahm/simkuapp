5e9880ba9ea00b6576e291264b7c4224a9ccdfd1
walidsj <mohwalidas@outlook.com>
2022-11-24 14:29:34 +0800

    update bootstrap

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,16 +2,20 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                Copyright © {{ date('Y') }} {{ config('app.meta.company') }} <span
-                    class="badge badge-pill badge-soft-success ml-2"><i
-                        class="fas fa-check mr-1 align-middle"></i><strong>{{ config('app.meta.name') }}</strong> |
-                    V{{ config('app.meta.version') }}</span>
+                Copyright © {{ date('Y') }} {{ config('app.meta.company') }}
+                {{-- <span
+                class="badge badge-pill badge-soft-success ml-2"><i
+                    class="fas fa-check mr-1 align-middle"></i><strong>{{ config('app.meta.name') }}</strong> |
+                V{{ config('app.meta.version') }}</span> --}}
             </div>
             <div class="col-sm-6">
 
                 <div class="text-sm-right d-block">
                     {{-- Dikembangkan oleh {{ config('app.meta.author_alias') }} --}}
-                    by <img src="{{ asset('img/walid-mark.png') }}" width="75.8" height="20" />
+                    {{-- by <img src="{{ asset('img/walid-mark.png') }}" width="75.8" height="20" /> --}}
+                    <span class="badge badge-pill badge-soft-success"><i
+                            class="fas fa-check mr-1 align-middle"></i><strong>{{ config('app.meta.name') }}</strong> |
+                        V{{ config('app.meta.version') }}</span>
                 </div>
             </div>
         </div>
